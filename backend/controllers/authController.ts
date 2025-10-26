44e93d0d690e73137966c4381551ff4c98192767
valex <mval.fx@gmail.com>
2025-10-26 10:46:37 +0100

    Set up signup route

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -4,9 +4,13 @@
 import {
   checkUsernameAvailability,
   checkEmailAvailability,
+  createUser,
 } from "../services/authServices";
 
 import { Request, Response, NextFunction } from "express";
+
+import bcrypt from "bcrypt";
+const SALT_ROUNDS = 10; // Typically a value between 10 and 12
 
 const signupController = async (
   req: Request,
@@ -35,12 +39,31 @@ const signupController = async (
       throw new Error("Email already exists");
     }
 
+    // Validate password strength
+    if (password.length < 6) {
+      throw new Error("Password must be at least 6 characters long");
+    }
+
     // Hash password
+    const bcryptPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    console.log("Bcrypt password:", bcryptPassword);
 
     // Insert user into database
+    const newUser = await createUser(username, email, bcryptPassword);
+
+    // Send result back to client
+    return res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
   } catch (error) {
-    console.error("Signup error:", error);
-    throw error;
+    // return res
+    //   .status(400)
+    //   .json({ message: "Signup failed", error: error.message });
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+    return res
+      .status(400)
+      .json({ message: "Signup failed", error: errorMessage });
   }
 };
 
