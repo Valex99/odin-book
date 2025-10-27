53087ac5ce14eb5326df6a5f32886e665fe57e2d
valex <mval.fx@gmail.com>
2025-10-27 11:45:47 +0100

    Set up everything to add user to DB

--- backend/server.ts
+++ backend/server.ts
@@ -3,12 +3,15 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
